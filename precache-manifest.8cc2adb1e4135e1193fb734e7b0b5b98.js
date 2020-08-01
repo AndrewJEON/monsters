9b7752ca9d12ecbc5b1019ff7813fad4f08eb599
Andrew JEON <andrew.jeon@funda.kr>
2020-08-01 17:58:02 +0900

    Updates

---
+++ precache-manifest.8cc2adb1e4135e1193fb734e7b0b5b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de11b1ba04dcbd1a662c0419978b86a8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d8564cd91fbbec7be6fc",
+    "url": "/monsters/static/css/main.812eb0f2.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8564cd91fbbec7be6fc",
+    "url": "/monsters/static/js/main.b1a3b602.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
